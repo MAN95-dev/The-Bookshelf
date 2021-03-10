5baaa4d58b640be4244b58368b345f05a38de134
MAN95-dev <michelleamynewell@gmail.com>
2021-03-10 15:30:18 +0000

    Add script.js

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+  
+  $('#bs-example-navbar-collapse-1')
+    .on('shown.bs.collapse', function() {
+      $('#navbar-hamburger').addClass('hidden');
+      $('#navbar-close').removeClass('hidden');    
+    })
+    .on('hidden.bs.collapse', function() {
+      $('#navbar-hamburger').removeClass('hidden');
+      $('#navbar-close').addClass('hidden');        
+    });
+  
+});
