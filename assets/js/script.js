f5485ddaf112dac13a577f09c1eefa7e371883c0
MAN95-dev <michelleamynewell@gmail.com>
2021-03-18 04:15:04 +0000

    Add fixed position to navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,8 @@ $(function() {
     }); 
 });
 
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+$('nav').affix({
+      offset: {
+        top: $('header').height()
+      }
+})
